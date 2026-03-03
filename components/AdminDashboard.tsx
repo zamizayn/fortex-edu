32dbe8a2e2558a399e0634db0b52e110a6dacdd3
aventusinformatics <shijin@indconsulting.com>
2026-03-03 14:16:10 +0530

    removed validation for college description and website url

--- components/AdminDashboard.tsx
+++ components/AdminDashboard.tsx
@@ -1775,7 +1775,6 @@ const AdminDashboard: React.FC<AdminDashboardProps> = ({ user, onLogout }) => {
                                     <div>
                                         <label className="block text-sm font-medium text-gray-700 mb-1">Website URL</label>
                                         <input
-                                            required
                                             type="url"
                                             className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                                             placeholder="https://ox.ac.uk"
@@ -1818,7 +1817,6 @@ const AdminDashboard: React.FC<AdminDashboardProps> = ({ user, onLogout }) => {
                                     <div className="md:col-span-2">
                                         <label className="block text-sm font-medium text-gray-700 mb-1">Description</label>
                                         <textarea
-                                            required
                                             rows={3}
                                             className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                                             placeholder="About this college..."
